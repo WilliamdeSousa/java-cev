f9aa4847e613f2a410548753bc1fa3ec4669356d
William de Sousa Costa <37248765+WIlliamdeSousa@users.noreply.github.com>
2023-04-01 17:31:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# java-cev
+ Alguns projetos do curso de Java do Curso em Vídeo
---
+++ mavenproject1/src/main/java/com/mmycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mmycompany.mavenproject1;
+
+/**
+ *
+ * @author costa
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
